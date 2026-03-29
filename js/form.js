80842c67b809570853bf50abb11117ca5bd6aee8
Justine Reinn Sanidad <namecode062@gmail.com>
2026-03-29 23:24:37 +0800

    i just modify password validation, add a email validation

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 
     (() => {
         'use strict'
+        const feedback = document.querySelector('.password-feedback');
+        const feedback2 = document.querySelector('.confirm-password-feedback');
+        const emailFeedback = document.querySelector('.email-feedback')
         const forms = document.querySelectorAll('.needs-validation');
         const email = document.querySelector('#inputEmail');
         const passwrd = document.querySelector('#password');
@@ -8,17 +11,40 @@
         Array.from(forms).forEach(form => {
             form.addEventListener('submit', event => {
                 let valid = form.checkValidity();
-                if (passwrd.value !== confirmPasswrd.value) {
-                    console.log("password is not correct")
-                    // remove Bootstrap valid styling
-                    passwrd.classList.remove('is-valid');
-                    confirmPasswrd.classList.remove('is-valid');
-
-                    // add invalid styling
+                if(passwrd.value !== "" && confirmPasswrd.value === "") {
+                    confirmPasswrd.classList.add('is-invalid');    
+                    console.log('1');
+                    feedback.style.display = 'none';
+                    feedback2.textContent = 'confirm password is empty';
+                    valid = false;
+                }else if(passwrd.value === "" && confirmPasswrd.value !== "") {
                     passwrd.classList.add('is-invalid');
-                    confirmPasswrd.classList.add('is-invalid');
+                    console.log('2');
+
+                    feedback2.style.display = 'none';
+                    feedback.style.display = 'block';
+                    feedback.textContent = 'password is empty';
+                    valid = false;
+                }else if (passwrd.value !== confirmPasswrd.value) {
+                    console.log('3');
+                    feedback.style.display = 'none';
+                    feedback2.style.display = 'block';
+                    feedback2.textContent = 'there is a typo between password or confirm password';
+                    // add invalid styling
+                     valid = false;
+                }else {
+                    console.log('4');
+                }
+                if (!email.value.includes('@') && email.value !== '') {
+                    emailFeedback.textContent = 'it should be valid email address';
+                    valid = false;
+                }else if (email.value === '') {
+                    emailFeedback.textContent = 'enter email address';
                     valid = false;
                 }
+                
+                
+                
                 if (!valid) {
                     
                     event.preventDefault();
